2e1af3e074dc6f53ca9949e90ef9eec50b0f0018
ayyoub <ayyoub@metriplica.com>
2019-05-24 09:30:11 +0200

    implementing streamer

--- pubsub_stream/__init__.py
+++ pubsub_stream/__init__.py
@@ -1,2 +1,15 @@
 import json
 from google.cloud import pubsub
+
+
+class Streaming(object):
+    def __init__(self):
+        pass
+
+    def read_data(self, data):
+        pass
+    
+
+if __name__ == '__main__':
+    print("hello world")
+    
