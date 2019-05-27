6c88c9a30703fb5f1576e66071a01eea06bbd633
ayyoub <ayyoub@metriplica.com>
2019-05-27 11:29:49 +0200

    Fixed streaming publisher

--- pubsub_stream/__init__.py
+++ pubsub_stream/__init__.py
@@ -15,7 +15,7 @@ class PubSubStreamer(object):
     def __init__(self, project_id, topic, speed=1):
         self.publisher = pubsub_v1.PublisherClient()
         self.topic = self.publisher.topic_path(project_id, topic)
-        self.device_id = os.getpid()
+        self.device_id = str(os.getpid())
         self.speed = speed
         products = pd.read_csv(os.path.join(dir_, "products.csv"))
         self.products = products.filter(["uniq_id", "product_name", "brand", "retail_price"])
@@ -25,18 +25,28 @@ class PubSubStreamer(object):
         
         while True:
             message = self.generate_message()
-            self.publisher.publish(self.topic, message, device_id=self.device_id, timestamp=time.time())
+            message_future = self.publisher.publish(self.topic, message, device_id=self.device_id, timestamp=str(time.time()))
+            message_future.add_done_callback(self.callback)
             time.sleep(1/self.speed)
 
+    @staticmethod
+    def callback(message_future):
+        if message_future.exception(timeout=30):
+            print('Publishing message threw an Exception {}.'.format(
+            message_future.exception()))
+        else:
+            print(message_future.result())
 
     
-    def generate_sample_order(self, products):
-        sample = products.sample(n=random.randint(1,10)).tolist()
+    def generate_sample_order(self):
+        sample = self.products.sample(n=random.randint(1,10))
         self.order_id = str(uuid.uuid4())
         self.order_iterator = sample.iterrows()
 
     def generate_message(self):
         try:
+            if not getattr(self, "order_iterator", None):
+                self.generate_sample_order()
             content = next(self.order_iterator)
             return self.format_message(content[1])
         except StopIteration:
--- pubsub_stream/__main__.py
+++ pubsub_stream/__main__.py
@@ -3,11 +3,11 @@ from pubsub_stream import PubSubStreamer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pub/Sub purchase demo streamer")
-    parser.add_argument("-p", "--project", dest="project_id", type=str, required=True, help="GCP project id.")
-    parser.add_argument("-s", "--speed", type=int, defualt=1, help="Speed of streaming")
-    parser.add_argument("topic", required=True, help="Pub/Sub topic to publish messages to.")
+    parser.add_argument("-p", "--project", dest="project_id", type=str, help="GCP project id.")
+    parser.add_argument("-s", "--speed", type=int, default=1, help="Speed of streaming")
+    parser.add_argument("topic", help="Pub/Sub topic to publish messages to.")
 
     args = parser.parse_args()
 
-    streamer = PubSubStreamer(**args)
+    streamer = PubSubStreamer(project_id=args.project_id, topic=args.topic, speed=args.speed)
     streamer()
